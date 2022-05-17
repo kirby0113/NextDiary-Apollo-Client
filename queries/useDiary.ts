89de7d1b621923162e26e8c6c2608699fcc01c2e
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-05-17 10:53:47 +0900

    fix: 型など微修正

--- queries/useDiary.ts
+++ queries/useDiary.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const GET_DIARIES = gql`
-  query getDiaries($getDiariesInput: GetDiariesInput) {
+  query getDiaries($getDiariesInput: GetDiariesInput!) {
     getDiaries(getDiariesInput: $getDiariesInput) {
       id
       title
