c9602ce9f2b5727cf3eba68820afdfefee2f960a
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-05-18 17:18:52 +0900

    fix: inputboxのプロパティ追加

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEventHandler } from "react";
 import styled from "styled-components";
 import { Colors } from "../constants/color";
 import { FontSize } from "../constants/font";
@@ -8,6 +8,8 @@ export type InputBoxProps = {
   boxSize?: keyof typeof InputPadding;
   fontSize?: keyof typeof FontSize;
   id?: string & String;
+  value: string;
+  onChange: (value: string) => void;
 };
 
 const StyledInputBox = styled.input<{ boxSize: keyof typeof InputPadding; fontSize: keyof typeof FontSize }>`
@@ -16,8 +18,23 @@ const StyledInputBox = styled.input<{ boxSize: keyof typeof InputPadding; fontSi
   font-size: ${({ fontSize }) => FontSize[fontSize]};
   width: 100%;
   border-radius: 6px;
+  letter-spacing: 1px;
 `;
 
-export const InputBox: React.FC<InputBoxProps> = ({ boxSize = "medium", fontSize = "medium", id }: InputBoxProps) => {
-  return <StyledInputBox boxSize={boxSize} fontSize={fontSize} id={id} />;
+export const InputBox: React.FC<InputBoxProps> = ({
+  boxSize = "medium",
+  fontSize = "medium",
+  id,
+  value,
+  onChange,
+}: InputBoxProps) => {
+  return (
+    <StyledInputBox
+      boxSize={boxSize}
+      fontSize={fontSize}
+      id={id}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
 };
