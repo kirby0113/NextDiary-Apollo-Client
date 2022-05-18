73722a0e8dabc3768c8e2577d35de7a9286e463b
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-05-18 12:58:38 +0900

    feat: いくつかの定数ファイルを作成

---
+++ constants/color.ts
@@ -0,0 +1,12 @@
+export const ButtonColor = {
+  white: {
+    background: "#fafafa",
+    border: "#333333",
+    text: "#333333",
+  },
+  black: {
+    background: "#333333",
+    border: "#000000",
+    text: "#ffffee",
+  },
+} as const;
---
+++ constants/padding.ts
@@ -0,0 +1,14 @@
+export const ButtonPadding = {
+  small: {
+    x: "5px",
+    y: "3px",
+  },
+  medium: {
+    x: "15px",
+    y: "7px",
+  },
+  large: {
+    x: "25px",
+    y: "15px",
+  },
+} as const;
