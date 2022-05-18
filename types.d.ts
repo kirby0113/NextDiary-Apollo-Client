9256eb2deb87aa1546bcfdfd466d92dd673e76c1
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-05-18 22:17:01 +0900

    feat: 型更新

--- types.d.ts
+++ types.d.ts
@@ -1,17 +1,11 @@
-import { GraphQLClient } from "graphql-request";
-import * as Dom from "graphql-request/dist/types.dom";
-import gql from "graphql-tag";
+import { GraphQLClient } from 'graphql-request';
+import * as Dom from 'graphql-request/dist/types.dom';
+import gql from 'graphql-tag';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]?: Maybe<T[SubKey]>;
-};
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]: Maybe<T[SubKey]>;
-};
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -24,48 +18,48 @@ export type Scalars = {
 };
 
 export type CreateDiaryInput = {
-  detail: Scalars["String"];
+  detail: Scalars['String'];
   tags: Array<InputTag>;
-  title: Scalars["ID"];
+  title: Scalars['ID'];
 };
 
 export type Diary = {
-  __typename?: "Diary";
-  created_at: Scalars["DateTime"];
-  detail: Scalars["String"];
-  id: Scalars["ID"];
+  __typename?: 'Diary';
+  created_at: Scalars['DateTime'];
+  detail: Scalars['String'];
+  id: Scalars['ID'];
   tags: Array<Tag>;
-  title: Scalars["String"];
+  title: Scalars['String'];
 };
 
 export type GetDiariesInput = {
-  order_asc?: InputMaybe<Scalars["Boolean"]>;
-  tag_id?: InputMaybe<Scalars["Float"]>;
+  order_asc?: InputMaybe<Scalars['Boolean']>;
+  tag_id?: InputMaybe<Scalars['Float']>;
 };
 
 export type InputTag = {
-  id: Scalars["ID"];
-  name: Scalars["String"];
-  user_id: Scalars["Float"];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  user_id: Scalars['Float'];
 };
 
 export type LoginResponse = {
-  __typename?: "LoginResponse";
-  access_token: Scalars["String"];
+  __typename?: 'LoginResponse';
+  access_token: Scalars['String'];
 };
 
 export type LoginUserInput = {
-  email: Scalars["String"];
-  password: Scalars["String"];
+  email: Scalars['String'];
+  password: Scalars['String'];
 };
 
 export type MessageResponse = {
-  __typename?: "MessageResponse";
-  message?: Maybe<Scalars["String"]>;
+  __typename?: 'MessageResponse';
+  message?: Maybe<Scalars['String']>;
 };
 
 export type Mutation = {
-  __typename?: "Mutation";
+  __typename?: 'Mutation';
   createDiary: Diary;
   createTag: Tag;
   createUser: User;
@@ -77,49 +71,56 @@ export type Mutation = {
   updateTag: Tag;
 };
 
+
 export type MutationCreateDiaryArgs = {
   createDiaryInput: CreateDiaryInput;
 };
 
+
 export type MutationCreateTagArgs = {
-  name: Scalars["String"];
+  name: Scalars['String'];
 };
+
 
 export type MutationCreateUserArgs = {
-  email: Scalars["String"];
-  name: Scalars["String"];
-  password: Scalars["String"];
+  email: Scalars['String'];
+  name: Scalars['String'];
+  password: Scalars['String'];
 };
+
 
 export type MutationDeleteDiaryArgs = {
-  id: Scalars["Float"];
+  id: Scalars['Float'];
 };
 
+
 export type MutationDeleteTagArgs = {
-  id: Scalars["Float"];
+  id: Scalars['Float'];
 };
+
 
 export type MutationLoginArgs = {
   loginUserInput: LoginUserInput;
 };
 
+
 export type MutationRegisterArgs = {
-  email: Scalars["String"];
-  name: Scalars["String"];
-  password: Scalars["String"];
+  registerUserInput: RegisterUserInput;
 };
+
 
 export type MutationUpdateDiaryArgs = {
   updateDiaryInput: UpdateDiaryInput;
 };
 
+
 export type MutationUpdateTagArgs = {
-  id: Scalars["Float"];
-  name: Scalars["String"];
+  id: Scalars['Float'];
+  name: Scalars['String'];
 };
 
 export type Query = {
-  __typename?: "Query";
+  __typename?: 'Query';
   getDiaries: Array<Diary>;
   getDiary: Diary;
   tags: Array<Tag>;
@@ -127,116 +128,106 @@ export type Query = {
   users: Array<User>;
 };
 
+
 export type QueryGetDiariesArgs = {
   getDiariesInput: GetDiariesInput;
 };
 
+
 export type QueryGetDiaryArgs = {
-  id: Scalars["Float"];
+  id: Scalars['Float'];
+};
+
+export type RegisterUserInput = {
+  email: Scalars['String'];
+  name: Scalars['String'];
+  password: Scalars['String'];
 };
 
 export type Tag = {
-  __typename?: "Tag";
-  id: Scalars["ID"];
-  name: Scalars["String"];
-  user_id: Scalars["Float"];
+  __typename?: 'Tag';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  user_id: Scalars['Float'];
 };
 
 export type UpdateDiaryInput = {
-  detail: Scalars["String"];
-  id: Scalars["ID"];
+  detail: Scalars['String'];
+  id: Scalars['ID'];
   tags: Array<InputTag>;
-  title: Scalars["String"];
+  title: Scalars['String'];
 };
 
 export type User = {
-  __typename?: "User";
-  email: Scalars["String"];
-  id: Scalars["ID"];
-  name: Scalars["String"];
-  password: Scalars["String"];
+  __typename?: 'User';
+  email: Scalars['String'];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  password: Scalars['String'];
 };
 
 export type DiariesQueryVariables = Exact<{
   getDiariesInput: GetDiariesInput;
 }>;
 
-export type DiariesQuery = {
-  __typename?: "Query";
-  getDiaries: Array<{ __typename?: "Diary"; id: string; title: string }>;
-};
+
+export type DiariesQuery = { __typename?: 'Query', getDiaries: Array<{ __typename?: 'Diary', id: string, title: string }> };
 
 export type DiaryQueryVariables = Exact<{
-  id: Scalars["Float"];
+  id: Scalars['Float'];
 }>;
 
-export type DiaryQuery = {
-  __typename?: "Query";
-  getDiary: { __typename?: "Diary"; id: string; title: string };
-};
+
+export type DiaryQuery = { __typename?: 'Query', getDiary: { __typename?: 'Diary', id: string, title: string } };
+
+export type RegisterMutationVariables = Exact<{
+  registerUserInput: RegisterUserInput;
+}>;
+
+
+export type RegisterMutation = { __typename?: 'Mutation', register: { __typename?: 'LoginResponse', access_token: string } };
+
 
 export const DiariesDocument = gql`
-  query Diaries($getDiariesInput: GetDiariesInput!) {
-    getDiaries(getDiariesInput: $getDiariesInput) {
-      id
-      title
-    }
+    query Diaries($getDiariesInput: GetDiariesInput!) {
+  getDiaries(getDiariesInput: $getDiariesInput) {
+    id
+    title
   }
-`;
+}
+    `;
 export const DiaryDocument = gql`
-  query Diary($id: Float!) {
-    getDiary(id: $id) {
-      id
-      title
-    }
+    query Diary($id: Float!) {
+  getDiary(id: $id) {
+    id
+    title
   }
-`;
+}
+    `;
+export const RegisterDocument = gql`
+    mutation Register($registerUserInput: RegisterUserInput!) {
+  register(registerUserInput: $registerUserInput) {
+    access_token
+  }
+}
+    `;
 
-export type SdkFunctionWrapper = <T>(
-  action: (requestHeaders?: Record<string, string>) => Promise<T>,
-  operationName: string,
-  operationType?: string
-) => Promise<T>;
+export type SdkFunctionWrapper = <T>(action: (requestHeaders?:Record<string, string>) => Promise<T>, operationName: string, operationType?: string) => Promise<T>;
 
-const defaultWrapper: SdkFunctionWrapper = (
-  action,
-  _operationName,
-  _operationType
-) => action();
 
-export function getSdk(
-  client: GraphQLClient,
-  withWrapper: SdkFunctionWrapper = defaultWrapper
-) {
+const defaultWrapper: SdkFunctionWrapper = (action, _operationName, _operationType) => action();
+
+export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = defaultWrapper) {
   return {
-    Diaries(
-      variables: DiariesQueryVariables,
-      requestHeaders?: Dom.RequestInit["headers"]
-    ): Promise<DiariesQuery> {
-      return withWrapper(
-        (wrappedRequestHeaders) =>
-          client.request<DiariesQuery>(DiariesDocument, variables, {
-            ...requestHeaders,
-            ...wrappedRequestHeaders,
-          }),
-        "Diaries",
-        "query"
-      );
+    Diaries(variables: DiariesQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<DiariesQuery> {
+      return withWrapper((wrappedRequestHeaders) => client.request<DiariesQuery>(DiariesDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'Diaries', 'query');
     },
-    Diary(
-      variables: DiaryQueryVariables,
-      requestHeaders?: Dom.RequestInit["headers"]
-    ): Promise<DiaryQuery> {
-      return withWrapper(
-        (wrappedRequestHeaders) =>
-          client.request<DiaryQuery>(DiaryDocument, variables, {
-            ...requestHeaders,
-            ...wrappedRequestHeaders,
-          }),
-        "Diary",
-        "query"
-      );
+    Diary(variables: DiaryQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<DiaryQuery> {
+      return withWrapper((wrappedRequestHeaders) => client.request<DiaryQuery>(DiaryDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'Diary', 'query');
     },
+    Register(variables: RegisterMutationVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<RegisterMutation> {
+      return withWrapper((wrappedRequestHeaders) => client.request<RegisterMutation>(RegisterDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'Register', 'mutation');
+    }
   };
 }
 export type Sdk = ReturnType<typeof getSdk>;
